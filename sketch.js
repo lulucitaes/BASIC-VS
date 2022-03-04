779cb1ffef22578218f3c18ebb534ed70c96e6a9
LULUCITAES <LOURDES.ESPINODELCASTILLO@GMAIL.COM>
2022-03-03 19:43:06 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+var trex ,trex_running;
+function preload(){
+  
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+ 
+}
+
+function draw(){
+  background("white")
+  
+
+}
